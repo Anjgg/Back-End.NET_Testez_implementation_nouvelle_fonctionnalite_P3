ffe720ad7a889578becb05a89eaca71a2f1e699e
Julien LEROY <julien.leroy.work@gmail.com>
2024-11-14 00:02:38 +0100

    Finished Unit Test for ProductViewModel

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -28,7 +28,143 @@ namespace P3.Tests
             var results = ValidateModel(model);
 
             // Assert
-            var expectedMessage = P3.Resources.Models.Services.ProductService.MissingName;
+            var expectedMessage = Resources.Models.Services.ProductService.MissingName;
+            Assert.Contains(results, res => res.ErrorMessage == expectedMessage);
+            Assert.Single(results);
+        }
+
+        [Theory]
+        [InlineData("en")]
+        [InlineData("fr")]
+        [InlineData("es")]
+        public void ProductViewModel_ShouldThrowError_WhenStockIsEmpty(string culture)
+        {
+            // Arrange
+            CultureInfo.CurrentUICulture = new CultureInfo(culture);
+            var model = new ProductViewModel
+            {
+                Name = "Name",
+                Stock = string.Empty,
+                Price = "1"
+            };
+            // Act
+            var results = ValidateModel(model);
+
+            // Assert
+            var expectedMessage = Resources.Models.Services.ProductService.MissingStock;
+            Assert.Contains(results, res => res.ErrorMessage == expectedMessage);
+            Assert.Single(results);
+        }
+
+        [Theory]
+        [InlineData("en")]
+        [InlineData("fr")]
+        [InlineData("es")]
+        public void ProductViewModel_ShouldThrowError_WhenStockNotAInteger(string culture)
+        {
+            // Arrange
+            CultureInfo.CurrentUICulture = new CultureInfo(culture);
+            var model = new ProductViewModel
+            {
+                Name = "Name",
+                Stock = "Stock",
+                Price = "1"
+            };
+            // Act
+            var results = ValidateModel(model);
+
+            // Assert
+            var expectedMessage = Resources.Models.Services.ProductService.StockNotAnInteger;
+            Assert.Contains(results, res => res.ErrorMessage == expectedMessage);
+        }
+
+        [Theory]
+        [InlineData("en")]
+        [InlineData("fr")]
+        [InlineData("es")]
+        public void ProductViewModel_ShouldThrowError_WhenStockNotGreaterThanZero(string culture)
+        {
+            // Arrange
+            CultureInfo.CurrentUICulture = new CultureInfo(culture);
+            var model = new ProductViewModel
+            {
+                Name = "Name",
+                Stock = "-1",
+                Price = "1"
+            };
+            // Act
+            var results = ValidateModel(model);
+
+            // Assert
+            var expectedMessage = Resources.Models.Services.ProductService.StockNotGreaterThanZero;
+            Assert.Contains(results, res => res.ErrorMessage == expectedMessage);
+            Assert.Single(results);
+        }
+
+        [Theory]
+        [InlineData("en")]
+        [InlineData("fr")]
+        [InlineData("es")]
+        public void ProductViewModel_ShouldThrowError_WhenPriceIsEmpty(string culture)
+        {
+            // Arrange
+            CultureInfo.CurrentUICulture = new CultureInfo(culture);
+            var model = new ProductViewModel
+            {
+                Name = "Name",
+                Stock = "1",
+                Price = string.Empty
+            };
+            // Act
+            var results = ValidateModel(model);
+
+            // Assert
+            var expectedMessage = Resources.Models.Services.ProductService.MissingPrice;
+            Assert.Contains(results, res => res.ErrorMessage == expectedMessage);
+            Assert.Single(results);
+        }
+
+        [Theory]
+        [InlineData("en")]
+        [InlineData("fr")]
+        [InlineData("es")]
+        public void ProductViewModel_ShouldThrowError_WhenPriceNotANumber(string culture)
+        {
+            // Arrange
+            CultureInfo.CurrentUICulture = new CultureInfo(culture);
+            var model = new ProductViewModel
+            {
+                Name = "Name",
+                Stock = "1",
+                Price = "Price"
+            };
+            // Act
+            var results = ValidateModel(model);
+
+            // Assert
+            var expectedMessage = Resources.Models.Services.ProductService.PriceNotANumber;
+            Assert.Contains(results, res => res.ErrorMessage == expectedMessage);
+        }
+
+        [Theory]
+        [InlineData("en")]
+        [InlineData("fr")]
+        [InlineData("es")]
+        public void ProductViewModel_ShouldThrowError_WhenPriceNotGreaterThanZero(string culture)
+        {
+            // Arrange
+            CultureInfo.CurrentUICulture = new CultureInfo(culture);
+            var model = new ProductViewModel
+            {
+                Name = "Name",
+                Stock = "1",
+                Price = "-1"
+            };
+            // Act
+            var results = ValidateModel(model);
+
+            // Assert
+            var expectedMessage = Resources.Models.Services.ProductService.PriceNotGreaterThanZero;
             Assert.Contains(results, res => res.ErrorMessage == expectedMessage);
             Assert.Single(results);
         }
@@ -37,7 +173,7 @@ namespace P3.Tests
         {
             var results = new List<ValidationResult>();
             var context = new ValidationContext(product);
-            Validator.TryValidateObject(product, context, results);
+            Validator.TryValidateObject(product, context, results, true);
             return results;
         }
     }
